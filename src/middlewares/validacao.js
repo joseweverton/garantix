00cc0485294257b30e884cc68d8fa345697d127d
jwcosta-br <jwcosta.br@gmail.com>
2025-06-28 21:32:55 -0300

    feat: verificarPermissaoCadastroUsuario para cadastro de novos usuarios.

--- src/middlewares/validacao.js
+++ src/middlewares/validacao.js
@@ -26,3 +26,24 @@ export const verificarEmailExistente = async (req, res, next) => {
 		return res.status(500).json({ mensagem: "Erro interno do servidor." });
 	}
 };
+
+export const verificarPermissaoCadastroUsuario = async (req, res, next) => {
+	try {
+		const usuario = await knex("usuarios").join("niveis_acesso", "usuarios.nivel_acesso_id", "niveis_acesso.id").where("usuarios.id", req.usuario.id).select("usuarios.admin", "niveis_acesso.nome as nivel_acesso").first();
+
+		if (!usuario) {
+			return res.status(404).json({ mensagem: "Usuário não encontrado." });
+		}
+
+		const temPermissao = usuario.admin === true || usuario.nivel_acesso.toLowerCase() === "alto";
+
+		if (!temPermissao) {
+			return res.status(403).json({ mensagem: "Acesso negado. Você não tem permissão para cadastrar novos usuários." });
+		}
+
+		next();
+	} catch (error) {
+		console.error("Erro no middleware de permissão:", error);
+		res.status(500).json({ mensagem: "Erro interno ao verificar permissões. Contacte o administrador" });
+	}
+};
--- src/rotas.js
+++ src/rotas.js
@@ -2,15 +2,14 @@ import { Router } from "express";
 import UsuariosController from "./controllers/usuarios.js";
 import { campoSenha, camposUsuarios, emailSenha } from "./middlewares/JOI/schemas.js";
 import autenticacaoLogin from "./middlewares/autenticacao.js";
-import { validarCorpoRequisicao, verificarEmailExistente } from "./middlewares/validacao.js";
+import { validarCorpoRequisicao, verificarEmailExistente, verificarPermissaoCadastroUsuario } from "./middlewares/validacao.js";
 
 const routes = Router();
-
-routes.post("/usuarios", validarCorpoRequisicao(camposUsuarios), verificarEmailExistente, UsuariosController.cadastrarUsuario);
 
 routes.post("/login", validarCorpoRequisicao(emailSenha), UsuariosController.login);
 // a partir daqui tem que ter o token validado
 routes.use(autenticacaoLogin);
+routes.post("/usuarios", verificarPermissaoCadastroUsuario, validarCorpoRequisicao(camposUsuarios), verificarEmailExistente, UsuariosController.cadastrarUsuario);
 routes.get("/usuarios", UsuariosController.DetalharPerfilUsuarios);
 routes.put("/usuarios", validarCorpoRequisicao(campoSenha), UsuariosController.editarPerfilUsuario);
 export default routes;
